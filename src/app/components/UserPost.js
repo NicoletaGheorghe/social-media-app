bb9cb6bfcf7d18f542b0930a909052dd56dfdc37
NicoletaGheorghe <g.nicole261193@gmail.com>
2025-05-11 16:52:29 +0100

    set likes count

--- src/app/components/UserPost.js
+++ src/app/components/UserPost.js
@@ -2,17 +2,21 @@
 import React from 'react'
 
 
-export const UserPost = ({userId, imageUrl, description, id, } ) => {
-  return (
+const UserPost = ({userId, imageUrl, description, id, handleLikes, likedPostCount, handleDislikes, dislikedPostCount} ) => {
+  
+  
+    return (
     <div className='flex flex-col md:w-7/8 mx-auto mt-6 shadow-md rounded-xl'>
         <h2 className="text-2xl font-bold place-self-center text-purple-800 p-2">{userId}</h2>
         <img src={imageUrl} className=" w-110 h-110 rounded-sm"></img>
         <p className='text-md font-medium p-4'>{description}</p>
-        <hr className='border-b-1 w-2/3 mx-auto' />
+        <hr className='border-b-1 border-purple-200 w-2/3 mx-auto' />
        <div className='grid grid-cols-2 justify-between p-4'>
-            <button > ❤️  </button>
-            <button >💔  </button>
+            <button className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 " onClick={() => handleLikes(id)}> ❤️  {likedPostCount || 0}</button>
+            <button className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 " onClick={() => handleDislikes(id)}>💔 {dislikedPostCount || 0} </button>
         </div> 
     </div>
   )
 }
+
+export default UserPost
